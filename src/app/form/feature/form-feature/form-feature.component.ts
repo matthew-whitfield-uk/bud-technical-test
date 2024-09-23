e9233631f667f8e93fe05aa820e957e8ffe61a7b
MatthewWhitfield91 <42498540+MatthewWhitfield91@users.noreply.github.com>
2024-09-23 16:39:16 +0100

    form state

--- src/app/form/feature/form-feature/form-feature.component.ts
+++ src/app/form/feature/form-feature/form-feature.component.ts
@@ -1,10 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup,  ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { FormHistoryState } from '../../data-access/form-state.model';
-import { redoFormState, undoFormState, updateFormState } from '../../data-access/form.actions';
-import { Observable } from 'rxjs';
+import {
+  redoFormState,
+  undoFormState,
+  updateFormState,
+} from '../../data-access/form.actions';
+import { Observable, take } from 'rxjs';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -12,78 +22,75 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './form-feature.component.html',
-  styleUrl: './form-feature.component.scss'
+  styleUrl: './form-feature.component.scss',
 })
 export class FormFeatureComponent implements OnInit, OnDestroy {
   dynamicForm: FormGroup;
-
   formState$: Observable<FormHistoryState>;
-
-  constructor(private fb: FormBuilder, private store: Store<{ form: FormHistoryState }>){
-    this.dynamicForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      interests: this.fb.array([])
-    });
-
-    this.formState$ = this.store.select('form');
-
-
-  }
-
-
   showUndo: boolean = false;
   showRedo: boolean = true;
 
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<{ form: FormHistoryState }>
+  ) {
+    this.dynamicForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      interests: this.fb.array([]),
+    });
+
+    this.formState$ = this.store.select('form');
+  }
+
   ngOnInit(): void {
-    this.formState$.subscribe((state: FormHistoryState) => {
+    this.initializeForm();
+    this.subscribeToFormState();
+  }
+
+  private initializeForm(): void {
+    this.formState$.pipe(take(1)).subscribe((state: FormHistoryState) => {
       if (state.present) {
         this.dynamicForm.patchValue({
           name: state.present.name,
-          email: state.present.email
+          email: state.present.email,
+          interests: state.present.interests || [],
         });
-
-        if (state.present.interests && state.present.interests.length) {
-          this.dynamicForm.patchValue({
-            interests: state.present.interests
-          });
-        }
       }
-
-      if(state.past && state.past.length > 0) {
-        this.showUndo = true;
-      } else {
-        this.showUndo = false;
-      }
-
-      if(state.future && state.future.length > 0) {
-        this.showRedo = true;
-      } else {
-        this.showRedo = false;
-      }
-
-
+      this.updateUndoRedoVisibility(state);
     });
   }
 
-  ngOnDestroy(): void {
-    const { name, email, interests } = this.dynamicForm.value;
-    this.store.dispatch(updateFormState({ name, email, interests }));
+  private subscribeToFormState(): void {
+    this.formState$.subscribe(this.updateUndoRedoVisibility.bind(this));
   }
 
-   undo() {
+  private updateUndoRedoVisibility(state: FormHistoryState): void {
+    this.showUndo = state.past && state.past.length > 0;
+    this.showRedo = state.future && state.future.length > 0;
+  }
+
+  ngOnDestroy(): void {
+    this.submitFormData();
+  }
+
+  undoFormState() {
     this.store.dispatch(undoFormState());
   }
 
-  redo() {
+  redoFormState() {
     this.store.dispatch(redoFormState());
   }
 
-
-
-  get name() { return this.dynamicForm.get('name'); }
-  get email() { return this.dynamicForm.get('email'); }
-  get interests() { return this.dynamicForm.get('interests') as FormArray; }
+  get name() {
+    return this.dynamicForm.get('name');
+  }
+  get email() {
+    return this.dynamicForm.get('email');
+  }
+  get interests() {
+    return this.dynamicForm.get('interests') as FormArray;
+  }
 
   addInterest() {
     this.interests.push(this.fb.control(''));
@@ -94,12 +101,13 @@ export class FormFeatureComponent implements OnInit, OnDestroy {
   }
 
   onSubmit() {
-
-    console.log('submit');
     if (this.dynamicForm.valid) {
-      const { name, email, interests } = this.dynamicForm.value;
-      this.store.dispatch(updateFormState({ name, email, interests }));
-
+      this.submitFormData();
     }
+  }
+
+  submitFormData() {
+    const { name, email, interests } = this.dynamicForm.value;
+    this.store.dispatch(updateFormState({ name, email, interests }));
   }
 }
