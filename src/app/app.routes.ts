04c15d7a4fe3ce0a016e9c7f3a69c269811813c8
MatthewWhitfield91 <42498540+MatthewWhitfield91@users.noreply.github.com>
2024-09-23 15:34:57 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FormFeatureComponent } from './form/feature/form-feature/form-feature.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: FormFeatureComponent
+  }
+];
--- src/app/form/feature/form-feature/form-feature.component.ts
+++ src/app/form/feature/form-feature/form-feature.component.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup,  ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-feature',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form-feature.component.html',
   styleUrl: './form-feature.component.scss'
 })
 export class FormFeatureComponent {
+  dynamicForm: FormGroup;
 
+
+  constructor(private fb: FormBuilder){
+    this.dynamicForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      interests: this.fb.array([])
+    });
+  }
+
+  get name() { return this.dynamicForm.get('name'); }
+  get email() { return this.dynamicForm.get('email'); }
+  get interests() { return this.dynamicForm.get('interests') as FormArray; }
+
+  addInterest() {
+    this.interests.push(this.fb.control(''));
+  }
+
+  removeInterest(index: number) {
+    this.interests.removeAt(index);
+  }
+
+  onSubmit() {
+    if (this.dynamicForm.valid) {
+      console.log(this.dynamicForm.value);
+      // Here you would typically send the form data to a server
+    }
+  }
 }
