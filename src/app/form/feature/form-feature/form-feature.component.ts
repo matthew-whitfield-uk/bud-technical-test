8a104e701d9545e728911a1321bb2e91fa311309
MatthewWhitfield91 <42498540+MatthewWhitfield91@users.noreply.github.com>
2024-09-23 14:54:18 +0100

    inital commit

---
+++ src/app/form/feature/form-feature/form-feature.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-feature',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-feature.component.html',
+  styleUrl: './form-feature.component.scss'
+})
+export class FormFeatureComponent {
+
+}
